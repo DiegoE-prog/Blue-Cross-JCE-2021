62a47400dbc18d57b2af9de60d7a9f80f0a17e6f
MauricioChav <77810855+MauricioChav@users.noreply.github.com>
2023-10-23 17:15:27 -0600

    Added Dependencies: Redux Toolkit, Axios, React-Router-DOM, Bootrstrap and Material UI. General configuration for React-router-dom and Redux Store

--- frontend/jce-app/src/index.js
+++ frontend/jce-app/src/index.js
@@ -1,6 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css"
 
+//Redux Store
+import { Provider } from "react-redux";
+import store from "./store";
+
+//React-Router-DOM
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render()
+root.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+)
