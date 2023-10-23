a6e616a73dc755720f333dee692975bc4e656db1
DiegoE-prog <s16120278@alumnos.itsur.edu.mx>
2023-10-23 16:50:40 -0600

    Login page without style created

---
+++ frontend/jce-app/src/components/Login/IndexLogin.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react"
+import BlueCrossLogo from "../../logos/BlueCrossLogo.png"
+
+function Index() {
+	const style = {
+		position: "absolute",
+		top: "50%",
+		left: "50%",
+		transform: "translate(-50%, -50%)"
+	}
+
+	const [user, setUser] = useState({
+		username: "",
+		password: ""
+	})
+
+	const handleChange = (e) => {
+		const value = e.target.value
+		setUser({
+			...user,
+			[e.target.name]: value
+		})
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		alert("This user is a test " + user.username + " " + user.password)
+	}
+
+	const resetFields = () => {
+		setUser({
+			username: "",
+			password: ""
+		})
+	}
+
+	return (
+		<div style={style}>
+			<div>
+				<img src={BlueCrossLogo} width={180} height={80} style={{ float: "left" }} alt="Blue Cross Logo" />
+			</div>
+			<form onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor="username">Username</label>
+					<input type="text" name="username" id="username" value={user.username} onChange={handleChange} />
+				</div>
+				<div>
+					<label htmlFor="password">Password</label>
+					<input type="password" name="password" id="password" value={user.password} onChange={handleChange} />
+				</div>
+				<div>
+					<button onClick={resetFields}>Reset</button>
+					<button type="submit">Login</button>
+				</div>
+			</form>
+		</div>
+	)
+}
+
+export default Index
