dacd7ff61a7054d51a51316649b163d76fff06ac
MauricioChav <77810855+MauricioChav@users.noreply.github.com>
2023-10-24 17:02:42 -0600

    Bluecross 22 and 21 Style adjustment

--- frontend/jce-app/src/views/profile.js
+++ frontend/jce-app/src/views/profile.js
@@ -6,56 +6,56 @@ function Profile(props) {
   }, [props.title]);
   return (
     <div style={{ margin: "20px" }}>
-      <h1>Profile</h1>
+      <h1 className="section-jce">Profile</h1>
 
       <div style={{ margin: "20px" }}>
-        <h2>Personal Information</h2>
+        <h2 className="header-jce">Personal Information</h2>
 
         <div className="content">
           <div className="row">
 
             <div className="col-4">
-              <h5>Name</h5>
+              <h5 className="general-jce">Name</h5>
               <input style={{ width: "100%" }} type="text" id="name" name="name"readOnly/>
             </div>
 
             <div className="col-4">
-              <h5>Last Name</h5>
+              <h5 className="general-jce">Last Name</h5>
               <input style={{ width: "100%" }} type="text" id="last_name" name="last_name" readOnly/>
             </div>
 
             <div className="col-4">
-              <h5>DOB</h5>
+              <h5 className="general-jce">DOB</h5>
               <input style={{ width: "100%" }} type="text" id="dob" name="dob" readOnly/>
             </div>
 
             <div className="col-4">
-              <h5>Phone Number</h5>
+              <h5 className="general-jce">Phone Number</h5>
               <input style={{ width: "100%" }} type="number" id="dob"name="dob"/>
             </div>
 
             <div className="col-8">
-              <h5>E-mail</h5>
+              <h5 className="general-jce">E-mail</h5>
               <input style={{ width: "100%" }} type="text" id="dob" name="dob"/>
             </div>
 
             <div className="col-4">
-              <button type="button" className="btn btn-secondary">
+              <button type="button" className="btn btn-blue">
                 Save
               </button>
             </div>
 
-            <h2>User Information</h2>
+            <h2 className="header-jce">User Information</h2>
             <div className="content">
               <div className="row">
 
                 <div className="col-4">
-                  <h5>User</h5>
+                  <h5 className="general-jce">User</h5>
                   <input style={{ width: "100%" }} type="text" id="user" name="user" readOnly/>
                 </div>
 
                 <div className="col-4">
-                  <h5>Role</h5>
+                  <h5 className="general-jce">Role</h5>
                   <input style={{ width: "100%" }} type="text" id="role" name="role" readOnly/>
                 </div>
 
