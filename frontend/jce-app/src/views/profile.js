a91be3fa5e22cfc490340954da1c3003dc073fd1
MauricioChav <77810855+MauricioChav@users.noreply.github.com>
2023-10-24 17:23:59 -0600

    Bluecross 22 and 21 Further Style adjustment and spaces

--- frontend/jce-app/src/views/profile.js
+++ frontend/jce-app/src/views/profile.js
@@ -11,60 +11,69 @@ function Profile(props) {
       <div style={{ margin: "20px" }}>
         <h2 className="header-jce">Personal Information</h2>
 
-        <div className="content">
+        <div className="content mb-4">
           <div className="row">
 
             <div className="col-4">
               <h5 className="general-jce">Name</h5>
-              <input style={{ width: "100%" }} type="text" id="name" name="name"readOnly/>
+              <input className="w-100" type="text" id="name" name="name"readOnly/>
             </div>
 
             <div className="col-4">
               <h5 className="general-jce">Last Name</h5>
-              <input style={{ width: "100%" }} type="text" id="last_name" name="last_name" readOnly/>
+              <input className="w-100" type="text" id="last_name" name="last_name" readOnly/>
             </div>
 
             <div className="col-4">
               <h5 className="general-jce">DOB</h5>
-              <input style={{ width: "100%" }} type="text" id="dob" name="dob" readOnly/>
+              <input className="w-100" type="text" id="dob" name="dob" readOnly/>
             </div>
+
+            <div className="col-12 mt-2">
+              <div className="row">
+                <div className="col-4">
+                  <h5 className="general-jce">Phone Number</h5>
+                  <input className="w-100" type="number" id="dob"name="dob"/>
+                </div>
+
+                <div className="col-8">
+                  <h5 className="general-jce">E-mail</h5>
+                  <input className="w-100" type="text" id="dob" name="dob"/>
+                </div>
+              </div>
+              
+            </div>
+
+            
 
             <div className="col-4">
-              <h5 className="general-jce">Phone Number</h5>
-              <input style={{ width: "100%" }} type="number" id="dob"name="dob"/>
-            </div>
-
-            <div className="col-8">
-              <h5 className="general-jce">E-mail</h5>
-              <input style={{ width: "100%" }} type="text" id="dob" name="dob"/>
-            </div>
-
-            <div className="col-4">
-              <button type="button" className="btn btn-blue">
+              <button type="button" className="btn btn-blue my-2">
                 Save
               </button>
             </div>
 
-            <h2 className="header-jce">User Information</h2>
+          </div>
+
+        </div>
+
+        <h2 className="header-jce">User Information</h2>
             <div className="content">
               <div className="row">
 
                 <div className="col-4">
                   <h5 className="general-jce">User</h5>
-                  <input style={{ width: "100%" }} type="text" id="user" name="user" readOnly/>
+                  <input className="w-100" type="text" id="user" name="user" readOnly/>
                 </div>
 
                 <div className="col-4">
                   <h5 className="general-jce">Role</h5>
-                  <input style={{ width: "100%" }} type="text" id="role" name="role" readOnly/>
+                  <input className="w-100" type="text" id="role" name="role" readOnly/>
                 </div>
 
               </div>
 
             </div>
 
-          </div>
-        </div>
       </div>
     </div>
   );
