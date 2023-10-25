5d1b041d0b06ac2b89768e14c260055d62f9c6df
DiegoE-prog <s16120278@alumnos.itsur.edu.mx>
2023-10-25 08:43:06 -0600

    Footer component created

---
+++ frontend/jce-app/src/nav/Footer.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Footer() {
+	return (
+		<footer>
+			<div className="row">
+				<div className="col-md-6">
+					<label className="general-jce">Powered by Experience Incubator</label>
+				</div>
+				<div className="col-md-6">
+					<label className="general-jce text-end">Copyright © 2021 Blue Cross JCE Health-Care. All rigths reserved.</label>
+				</div>
+			</div>
+		</footer>
+	)
+}
+
+export default Footer
