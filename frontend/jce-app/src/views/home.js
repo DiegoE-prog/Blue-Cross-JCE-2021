9a12a2bf28c4ed2ad77e5d44cbbdb40602acf481
DiegoE-prog <s16120278@alumnos.itsur.edu.mx>
2023-12-01 12:05:49 -0600

    Added pic to Home Page

--- frontend/jce-app/src/views/home.js
+++ frontend/jce-app/src/views/home.js
@@ -1,13 +1,15 @@
 import React, { useEffect } from "react"
+import HomePicJCE from "../logos/HomePicJCE.png"
 
 function Home(props) {
 	useEffect(() => {
 		document.title = props.title
 	}, [props.title])
-	
+
 	return (
 		<div className="container">
 			<h1>Home Page</h1>
+			<img src={HomePicJCE} style={{ width: 800, height: 450 }} />
 		</div>
 	)
 }
